13f38b45a2df0abf1ffcc1ba27bc320ec79c78ed
Alba <albahaxhimali1@gmail.com>
2019-07-23 12:12:24 +0200

    --add: all files

---
+++ components/loadingBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="loading-container">
+
+  </div>
+</template>
+<style lang="scss" scoped>
+  .loading-container {
+    width: 250px;
+    height: 250px;
+    border-radius: 50%;
+    box-sizing: border-box;
+    border-top: 10px solid #f1c40f;
+    position: relative;
+    animation: load 2s linear infinite;
+
+    &:before, &:after {
+      content: '';
+      width: 250px;
+      height: 250px;
+      position: absolute;
+      left: 0;
+      top: -12px;
+      border-radius: 50%;
+      box-sizing: border-box;
+    }
+
+    &:before {
+      border-top: 10px solid #e74c3c;
+      transform: rotate(120deg);
+    }
+
+    &:after {
+      border-top: 10px solid #1abc9c;
+      transform: rotate(240deg);
+    }
+    
+    @keyframes load {
+      to {
+        transform: rotate(360deg);
+      }
+    }
+  }
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <loading-bar-component></loading-bar-component>
+  </div>
+</template>
+<script>
+  import loadingBarComponent from '@/components/loadingBar'
+  export  default  {
+    components:  {
+      loadingBarComponent
+    }
+  }
+</script>
